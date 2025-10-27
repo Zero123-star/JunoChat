7507fe2afa93c4619cc3517fb1bef745f937246d
Zero123-star <prizlopaniustin@yahoo.ro>
2025-10-27 16:45:15 +0200

    Added comments

--- game/inventory.py
+++ game/inventory.py
@@ -4,11 +4,14 @@ class Inventory:
     def __init__(self):
         self.items = []
         self.equipped_items = dict
+        ### Add debugging and try throw catch for when functions are called.
+        # We will need when we will move to building the actual combat system
+        # Also please add returns
 
-    def add_item(self, item: Item):
+    def add_item(self, item: Item): ### Return the added item
         self.items.append(item)
 
-    def remove_item(self, item: Item):
+    def remove_item(self, item: Item): ##Return the removed item. We will probably need that when we use potions
         if item in self.items:
             self.items.remove(item)
 
@@ -18,14 +21,16 @@ class Inventory:
     def list_items(self):
         return [item.name for item in self.items]
     
-    def equip_item(self, item: Item):
+    def equip_item(self, item: Item): ###Return the equipped item
         if item in self.items:
             self.equipped_items[item.slot] = item
             
-    def unequip_item(self, slot: str):
+    def unequip_item(self, slot: str): ### Return the unequiped item
         if slot in self.equipped_items:
-            del self.equipped_items[slot]
-            
+            del self.equipped_items[slot] ####Does it return in the self.items? 
+
+
+
     def get_equipped_items(self):
         return self.equipped_items
     
--- game/item.py
+++ game/item.py
@@ -24,12 +24,42 @@ class StaffType(Enum):
 
 
 class Item:
-
     def __init__(self, name: str, type: ItemType, subtype: WeaponType | ArmorType | StaffType | None = None):
         self.name = name
         self.type = type
         self.subtype = subtype
+        self.stats= dict
+        ### self.slot 
+        ### Items currently do not have slots
+
+
+        ###Each item needs to have stats given to whoever equips them: defense, attack, dexterity, agility etc
+        # Probably add an item description too
+        # Make sure that items actually use slots
+        #
+        # Add a set of potion like items, healing potions, mana potions, buff potions, who don't equip a slot but will be used
+        # Make sure stats of an item follow the format of a Stat in the stats.py class(healing/mana potions dont need to). 
+        # Something like: 
+        # item1: self.name=greatsword 
+        #        self.type=WeaponType
+        #        self.slot=(Left_hand,Righ_hand) ###Lets go with some items occupy more slots. 
+        #        self.stats={"Attack" : 500}
+        # item2: self.name=Plate_Armor
+        #        self.type=WeaponType
+        #        self.slot=(Chest) 
+        #        self.stats={"Defense" : 500, "Agility" : -50} //Less agile from heavy armor
         
+        
+        # For stat class logic:
+        # Armor/weapon type items would have fall into temporary_stats, but the value_and_time class should have a parameter 'permanent'
+        # When a turn passes, if you have your armor/weapon equipped its remaining_turns value will not decrease
+        # Since the item is equipped
+        #
+        #
+        # Please add some item equipping removing tests too. Obviously using inventory class too
+        # Thx
+
+
     def get_name(self) -> str:
         return self.name
     
